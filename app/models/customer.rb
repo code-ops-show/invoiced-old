7c18e0a743bd63f33c87c38bd7044d79e988369f
suparerk <ch.suparerk@gmail.com>
2015-06-16 17:44:45 +0700

    indexing completed

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,9 @@
 class Customer < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+  include Customers::Indexing
 
   belongs_to :user
   has_many :invoices, dependent: :destroy
+
 end
