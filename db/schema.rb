5f8d590a57c1cd892468ea8af02d3b334df33a0c
bestbase32 <kero_sos@hotmail.com>
2015-07-17 11:11:00 +0700

    add extra_vat to DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714095138) do
+ActiveRecord::Schema.define(version: 20150716110042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150714095138) do
     t.string   "method"
     t.integer  "invoice_id"
     t.integer  "row_order"
+    t.decimal  "extra_vat"
   end
 
   add_index "extras", ["invoice_id"], name: "index_extras_on_invoice_id", using: :btree
